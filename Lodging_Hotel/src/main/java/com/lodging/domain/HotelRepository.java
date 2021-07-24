e1d8be411b420dd6dfc246c6639292860d0fe40a
vmadala <vmadala@VMADALA-PC.pkrmsezdc.com>
2021-07-24 21:04:25 +0530

    E1

--- Lodging_Hotel/src/main/java/com/lodging/domain/HotelRepository.java
+++ Lodging_Hotel/src/main/java/com/lodging/domain/HotelRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
+public interface HotelRepository extends JpaRepository<T, ID> {
 
 }
